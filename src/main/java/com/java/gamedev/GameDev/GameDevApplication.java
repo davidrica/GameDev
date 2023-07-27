0c81bb10f0b473239a46da7fc529bc94a815a4cf
David Ricardo Torres <torresdavidricardo.red@gmail.com>
2023-07-27 12:14:14 -0300

    Inicializacion del Projecto GameDev

---
+++ src/main/java/com/java/gamedev/GameDev/GameDevApplication.java
@@ -0,0 +1,13 @@
+package com.java.gamedev.GameDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/gamedev/GameDev/GameDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.gamedev.GameDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
